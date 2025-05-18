aa1f50b5071bed50088e0d43b5b2a65cccbeb171
Lexavorio1 <leoboy99@mail.ru>
2025-05-18 16:22:31 +0300

    ready

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Field } from './components'
 import { Info } from './components'
 import { store } from './store'
+import { Component } from "react";
 
 export const App = () => {
   const restart = () => {
@@ -17,4 +18,23 @@ export const App = () => {
       </div>
 
   )
+}
+
+export class OldApp extends Component {
+  constructor (props) {
+    super(props)
+  }
+  restart() {
+      store.dispatch({ type: 'RESTART_GAME' })
+  }
+  render() {
+    return (
+      <div className="flex flex-col text-center justify-center g-[15px]">
+        <Info />
+        <button className='border-black border-solid border-2 m-[20px] outline outline-offset-2 outline-blue-500'
+         onClick={this.restart}>Начать заново</button>
+        <Field />
+      </div>
+    )
+  }
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import './index.css'
-import { App } from './App.jsx'
+import {/* App*/ OldApp } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <OldApp />
     </Provider>
   </StrictMode>,
 )
